1c83ea113798209a7034b5acc7d4df454bea71a0
mkchalla <madan.ch@gmail.com>
2021-01-06 16:18:39 +1100

    UI-WEB: change the styling for the Login and SignUp pages

--- ui-web/src/views/Login.vue
+++ ui-web/src/views/Login.vue
@@ -1,46 +1,119 @@
 <template>
-    <div class="login">
-        <form @submit.prevent="submit">
-            <div>
-                <label for="username">Username:</label>
-                <input type="text" name="username" v-model="form.username">
+  <div class="login">
+    <div class="container has-text-centered">
+      <div class="column is-4 is-offset-4">
+        <h3 class="title has-text-black">Login</h3>
+        <hr class="login-hr" />
+        <p class="subtitle has-text-black">Please login to proceed.</p>
+        <div class="box">
+          <figure class="avatar">
+            <img class="logo" src="../assets/user.png" />
+          </figure>
+          <form @submit.prevent="submit">
+            <div class="field">
+              <div class="control">
+                <input
+                  class="input is-large"
+                  type="email"
+                  v-model="user.email"
+                  placeholder="Your Email"
+                  autofocus=""
+                />
+              </div>
             </div>
-        </form>
-        <div>
-            <label for="password">Password:</label>
-            <input type="password" name="password" v-model="form.password">
+
+            <div class="field">
+              <div class="control">
+                <input
+                  class="input is-large"
+                  type="password"
+                  v-model="user.password"
+                  placeholder="Your Password"
+                />
+              </div>
+            </div>
+            <div class="field has-text-left">
+              <label class="checkbox">
+                <input type="checkbox" />
+                Remember me
+              </label>
+            </div>
+            <button class="button is-block is-info is-large is-fullwidth">
+              Login
+              <!-- <font-awesome-icon icon="address-card" /> -->
+            </button>
+          </form>
         </div>
-        <button type="submit">Sign Up</button>
-        <p v-if="showError" id="error">Username already exists</p>
+        <p class="has-text-grey">
+          <router-link to="/register">Sign Up</router-link> &nbsp;·&nbsp;
+          <a href="../">Forgot Password</a> &nbsp;·&nbsp;
+          <a href="../">Need Help?</a>
+        </p>
+      </div>
     </div>
+
+    <!-- 
+        <form @submit.prevent="submit">
+            <div class="columns">
+                <div class="column is-4 is-offset-4">
+                    <div>
+                        <label for="username">Username:</label>
+                        <input type="text" name="username" v-model="user.username">
+                    </div>
+                    <div>
+                        <label for="password">Password:</label>
+                        <input type="password" name="password" v-model="user.password">
+                    </div>
+                    <button type="submit">Sign Up</button>
+                    <p v-if="showError" id="error">Username already exists</p>
+                </div>
+            </div>            
+        </form>  -->
+  </div>
 </template>
 
 <script>
 import { mapActions } from "vuex";
-export default {
-    name: 'login',
-    components: {},
-    data() {
-        return {
-            form: {
-                username: "",
-                password: ""
-            },
-            showError: false
-        }
-    },
-    methods: {
-        ...mapActions(["login"]),
-        async submit() {
+import User from '@/models/user'
+import { library } from "@fortawesome/fontawesome-svg-core";
+// import { faSpinner, faAlignLeft } from '@fortawesome/free-solid-svg-icons'
+import { faAddressCard } from "@fortawesome/free-solid-svg-icons";
 
-            try {
-                await this.login(this.form);
-                this.$router.push('/items');
-                this.showError = false;
-            } catch (error) {
-                this.showError = true
-            }
-        }
-    }
-}
+library.add(faAddressCard);
+
+export default {
+  name: "login",
+  components: {},
+  data() {
+    return {
+      user: new User('',''),
+      showError: false,
+    };
+  },
+  methods: {
+    ...mapActions(["login"]),
+    async submit() {
+      try {          
+        await this.login(this.user);
+        this.$router.push("/items");
+        this.showError = false;
+      } catch (error) {
+        this.showError = true;
+      }
+    },
+  },
+  computed: {},
+};
 </script>
+
+<style lang="scss" scoped>
+.avatar {
+  img.logo {
+    width: 128px;
+    height: 128px;
+  }
+}
+.has-text-left {
+  text-align: left;
+}
+</style>
--- ui-web/src/views/Register.vue
+++ ui-web/src/views/Register.vue
@@ -1,36 +1,101 @@
 <template>
-    <div class="register">
+  <div class="login">
+    <div class="container has-text-centered">
+      <div class="column is-4 is-offset-4">
+        <h3 class="title has-text-black">SignUp</h3>
+        <hr class="login-hr" />
+        <p class="subtitle has-text-black">Please provide your details to SignUp.</p>
+        <div class="box">
+          <form @submit.prevent="submit">
+            <div class="field">
+              <div class="control">
+                <input
+                  class="input is-large"
+                  type="email"
+                  v-model="user.email"
+                  placeholder="Enter Email"
+                  autofocus=""
+                />
+              </div>
+            </div>
+            <div class="field">
+              <div class="control">
+                <input
+                  class="input is-large"
+                  type="text"
+                  v-model="user.full_name"
+                  placeholder="Full Name"
+                  autofocus=""
+                />
+              </div>
+            </div>
+            <div class="field">
+              <div class="control">
+                <input
+                  class="input is-large"
+                  type="password"
+                  v-model="user.password"
+                  placeholder="Enter Password"
+                />
+              </div>
+            </div>
+            <div class="field">
+              <div class="control">
+                <input
+                  class="input is-large"
+                  type="password"
+                  v-model="user.confirm_password"
+                  placeholder="Confirm Password"
+                />
+              </div>
+            </div>
+            <button class="button is-block is-primary is-large is-fullwidth">
+              SignUp 
+              <!-- <font-awesome-icon icon="address-card" /> -->
+            </button>
+          </form>
+          <p v-if="showError" id="error">Username already exists</p>
+        </div>
+        <p class="has-text-grey">
+            
+          <router-link to="/register">SignUp</router-link> &nbsp;·&nbsp;
+          <a href="../">Forgot Password</a> &nbsp;·&nbsp;
+          <a href="../">Need Help?</a>
+        </p>
+      </div>
+    </div>
+  </div>
+
+
+    <!-- <div class="register">
         <form @submit.prevent="submit">
             <div>
                 <label for="username">Username:</label>
-                <input type="text" name="username" v-model="form.username">
+                <input type="text" name="username" v-model="user.username">
             </div>
         </form>
         <div>
             <label for="full_name">Full Name:</label>
-            <input type="text" name="full_name" v-model="form.full_name">
+            <input type="text" name="full_name" v-model="user.full_name">
         </div>
         <div>
             <label for="password">Password:</label>
-            <input type="password" name="password" v-model="form.password">
+            <input type="password" name="password" v-model="user.password">
         </div>
         <button type="submit">Sign Up</button>
         <p v-if="showError" id="error">Username already exists</p>
-    </div>
+    </div> -->
 </template>
 
 <script>
 import { mapActions } from "vuex";
+import User from '@/models/user';
 export default {
     name: 'register',
     components: {},
     data() {
         return {
-            form: {
-                username: "",
-                full_name: "",
-                password: ""
-            },
+            user: new User('', ''),
             showError: false
         }
     },
@@ -39,9 +104,9 @@ export default {
         async submit() {
 
             try {
-                await this.register(this.form);
-                this.$router.push('/items');
                 this.showError = false;
+                await this.register(this.user);
+                this.$router.push('/items');
             } catch (error) {
                 this.showError = true
             }
@@ -49,3 +114,10 @@ export default {
     }
 }
 </script>
+
+<style lang="scss" scoped>
+#error{
+    background-color: #f1c3c3;
+    color: red;
+}
+</style>
