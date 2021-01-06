c95c47073def1548b66e9212a384ae8e6902fee2
mkchalla <madan.ch@gmail.com>
2021-01-06 23:37:46 +1100

    UI-WEB: login success to store the username and token

--- ui-web/src/services/auth-header.js
+++ ui-web/src/services/auth-header.js
@@ -4,8 +4,8 @@ import store from '@/store/modules'
 export default function authHeader() {
     let user = store.getters.currentUser;
 
-    if (user && user.accessToken) {
-        return { Authorization: 'Bearer ' + user.accessToken };
+    if (user && user.access_token) {
+        return { Authorization: 'Bearer ' + user.access_token };
     } else {
         return {};
     }
--- ui-web/src/services/auth-service.js
+++ ui-web/src/services/auth-service.js
@@ -6,15 +6,8 @@ class AuthService {
   login(user, headers) {
     return axios
       .post(`${API_ROOT_URL}/login`, user, headers)
-      .then(response => {
-        const userData = {
-            username: user.username
-        }
-        
-        if (response.data.access_token) {
-            userData['accessToken'] = response.data.access_token;
-        }
-        return userData;
+      .then(response => {        
+        return response.data;
       });
   }
 
