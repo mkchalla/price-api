bea888812ee95d9f52acd64c66e0fbb33bffee1b
mkchalla <madan.ch@gmail.com>
2021-01-13 00:47:04 +1100

    API: update models with Product and PriceItem

--- api/app/models.py
+++ api/app/models.py
@@ -1,4 +1,10 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy import Boolean
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy import DateTime
+from sqlalchemy import Float
 from sqlalchemy.orm import relationship
 from sqlalchemy_utils import EmailType
 from datetime import datetime
@@ -15,3 +21,28 @@ class User(Base):
     username = Column(String, unique=True)
     hashed_password = Column(String)
     is_active = Column(Boolean, default=False)
+
+    price_items = relationship("PriceItem", back_populates="user")
+
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    description = Column(String)
+    image = Column(String)
+
+    price_items = relationship("PriceItem", back_populates="product")
+
+class PriceItem(Base):
+    __tablename__ = "price_item"
+
+    id = Column(Integer, primary_key=True)
+    updated = Column(DateTime, default=datetime.utcnow)
+    price = Column(Float(precision=2))
+    product_id = Column(Integer, ForeignKey("products.id"))
+    user_id = Column(Integer, ForeignKey("users.id"))
+
+    product = relationship("Product", back_populates="price_items")
+    user = relationship("User", back_populates="price_items")
