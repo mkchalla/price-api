7a207077f98e9dfa74048614d2ecb8269088bf6f
mkchalla <madan.ch@gmail.com>
2021-01-28 22:37:46 +1100

    API: define schemas for products in ORM mode

--- api/app/schemas.py
+++ api/app/schemas.py
@@ -1,22 +1,34 @@
 from typing import List, Optional
+from datetime import datetime
 
-from pydantic import BaseModel
+from pydantic import BaseConfig, BaseModel
 
 
-class Token(BaseModel):
+class APIModel(BaseModel):
+    class Config(BaseConfig):
+        orm_mode = True
+
+class Token(APIModel):
     access_token: str
     token_type: str
 
 
-class TokenData(BaseModel):
+class TokenData(APIModel):
     username: Optional[str] = None
 
 class TokenUser(Token):
     username: Optional[str] = None
 
+class PriceItem(APIModel):
+    price: float
+    udpated: datetime
+    user_id: int
+    product_id: int
 
-class User(BaseModel):
+
+class User(APIModel):
     username: str
+    price_items: List[PriceItem] = []
 
 
 class UserInDB(User):
@@ -25,3 +37,14 @@ class UserInDB(User):
 class UserCreate(User):
     password: str
     email: str
+
+class ProductBase(APIModel):
+    name: str
+    description: Optional[str]
+
+class ProductInDB(ProductBase):
+    image: Optional[str]
+
+
+class Product(ProductBase):
+    price_items: List[PriceItem] = []
