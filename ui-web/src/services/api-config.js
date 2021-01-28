7f324788201884a80212b0da2434fbb728f2c2ef
mkchalla <madan.ch@gmail.com>
2021-01-28 22:34:08 +1100

    UI-WEB: helper config file for root URL

--- ui-web/src/services/api-config.js
+++ ui-web/src/services/api-config.js
@@ -1,5 +1,7 @@
 const API_ROOT_URL = 'http://localhost:8000/api/v1'
 
+const _api_media_root_url = 'http://localhost:8000/media'
 
+export const API_MEDIA_ROOT = _api_media_root_url;
 
 export default API_ROOT_URL;
